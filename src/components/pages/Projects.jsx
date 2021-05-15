dbd65aa7d63248d9a2518f127f99265b72f49f6c
anat <anat.dean@verse.co.uk>
2021-05-15 11:04:28 +0100

    update projects

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,36 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
-import { rem } from 'polished';
-
-const Text = styled.p`
-  font-size: ${rem('25px')};
-  text-decoration: ${({ title }) => title && 'underline'};
-`;
-
-const Title = styled.h1`
-  font-weight: bold;
-  font-size: ${rem('30px')};
-  text-decoration: underline;
-`;
+import React from "react";
+import { rem } from "polished";
+import { Title, SubTitle, Text } from "../atoms";
 
 const Projects = () => (
   <section>
     <Title>Recent Projects</Title>
-    <Text title={true}>Curriculum Application (Northcoders Notes):</Text>
+    <SubTitle fontSize={rem("35px")}>
+      Resorts World Birmingham: Booking System
+    </SubTitle>
+    <Text>
+      Stack: React, MobX, React Hooks, SCSS, Formik, Yup, GraphQL, Jest,
+      CraftCMS , PHP,
+    </Text>
+    <Text>
+      Rebuilt a complex client booking system front end with intuitive and fast
+      UI. Involved building a CraftCMS data layer to allow the client to
+      customise it to their needs and integration with the booking API which
+      required a PHP proxy server as a relay between the API and frontend.
+    </Text>
+    <SubTitle fontSize={rem("35px")}>Curriculum Application:</SubTitle>
     <Text>
       Stack: React Hooks, React Testing Library, Jest, GraphQL, Firebase, OAuth,
       Styled Components, Github Apps
     </Text>
     <Text>
-      What: Fully authenticated site to interact with existing curriculum
-      resources and display them to students based on their current position on
-      their course.
-    </Text>
-    <Text title={true}>HILS System</Text>
-    <Text>Stack: Python, Django, HTML, CSS, Elastic Beanstalk, EC2, RDS</Text>
-    <Text>
-      What: Providing maintenance and working on tickets to a charity
-      infrastructure on a contractual basis.
+      Fully authenticated site to interact with existing curriculum resources
+      and display them to students based on their current position on their
+      course.
     </Text>
   </section>
 );
