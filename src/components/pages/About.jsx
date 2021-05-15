d6451c371147a5df7cb1c5b2d24983770beac6b7
anat <anat.dean@verse.co.uk>
2021-05-15 11:41:32 +0100

    open noreferrer links

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,9 +5,19 @@ const About = () => (
   <section>
     <Title>About Me</Title>
     <Text>
-      Full Stack Developer at <Link href="https://www.verse.co.uk/">Verse</Link>
+      Full Stack Developer at{" "}
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.verse.co.uk/"
+      >
+        Verse
+      </Link>
     </Text>
-    <Text>I work on adding features large and small to ReactJS projects.</Text>
+    <Text>
+      I build projects using ReactJS frontend and backends using any or all of
+      SQL, PHP, ExpressJS, Craft CMS
+    </Text>
     <Text>
       On a standard day you'll find me collaborating in a cross functional team,
       attending standups and meetings, demoing work to clients and developing
@@ -15,7 +25,14 @@ const About = () => (
     </Text>
     <Text>
       I am currently teaching myself TypeScript and am on a part time DevOps
-      course with tech returners.
+      course with{" "}
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.techreturners.com/"
+      >
+        Tech Returners.
+      </Link>
     </Text>
     <Text> Oh, and I do love a good refactor.</Text>
   </section>
