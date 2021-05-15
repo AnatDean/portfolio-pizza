e1b84473060fe59daff0bac18f41112e5aa3a190
anat <anat.dean@verse.co.uk>
2021-05-15 11:41:16 +0100

    update project text

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { rem } from "polished";
-import { Title, SubTitle, Text } from "../atoms";
+import { Title, SubTitle, Text, Link } from "../atoms";
 
 const Projects = () => (
   <section>
@@ -18,6 +18,18 @@ const Projects = () => (
       customise it to their needs and integration with the booking API which
       required a PHP proxy server as a relay between the API and frontend.
     </Text>
+    <Link href="">
+      You can find it here (pls don't actually make a booking though)
+    </Link>
+    <SubTitle fontSize={rem("35px")}>Bechdel Films</SubTitle>
+    <Text>Stack: React, NextJS, React Hooks, SCSS, Contentful</Text>
+    <Text>
+      Rebuilt a complex client booking system front end with intuitive and fast
+      UI. Involved building a CraftCMS data layer to allow the client to
+      customise it to their needs and integration with the booking API which
+      required a PHP proxy server as a relay between the API and frontend.
+    </Text>
+    <Link href="">Hosted here on firebase</Link>
     <SubTitle fontSize={rem("35px")}>Curriculum Application:</SubTitle>
     <Text>
       Stack: React Hooks, React Testing Library, Jest, GraphQL, Firebase, OAuth,
