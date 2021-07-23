759dd6aef983e26731f43da558d7ef3ea4140ac5
anat <anat.dean@verse.co.uk>
2021-07-23 17:23:59 +0100

    laptop backup

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { rem } from "polished";
-import { Title, SubTitle, Text, Link } from "../atoms";
+import { Title, SubTitle, Text } from "../atoms";
 
 const Projects = () => (
   <section>
@@ -13,32 +13,19 @@ const Projects = () => (
       CraftCMS , PHP,
     </Text>
     <Text>
-      Rebuilt a complex client booking system front end with intuitive and fast
+      Rebuilt a complex legacy booking system front end with intuitive and fast
       UI. Involved building a CraftCMS data layer to allow the client to
       customise it to their needs and integration with the booking API which
       required a PHP proxy server as a relay between the API and frontend.
     </Text>
-    <Link href="">
-      You can find it here (pls don't actually make a booking though)
-    </Link>
-    <SubTitle fontSize={rem("35px")}>Bechdel Films</SubTitle>
-    <Text>Stack: React, NextJS, React Hooks, SCSS, Contentful</Text>
-    <Text>
-      Rebuilt a complex client booking system front end with intuitive and fast
-      UI. Involved building a CraftCMS data layer to allow the client to
-      customise it to their needs and integration with the booking API which
-      required a PHP proxy server as a relay between the API and frontend.
-    </Text>
-    <Link href="">Hosted here on firebase</Link>
     <SubTitle fontSize={rem("35px")}>Curriculum Application:</SubTitle>
     <Text>
       Stack: React Hooks, React Testing Library, Jest, GraphQL, Firebase, OAuth,
       Styled Components, Github Apps
     </Text>
     <Text>
-      Fully authenticated site to interact with existing curriculum resources
-      and display them to students based on their current position on their
-      course.
+      Fully authenticated site to interact with Northcoders curriculum resources
+      and display them to students with multiple tiers of access.
     </Text>
   </section>
 );
